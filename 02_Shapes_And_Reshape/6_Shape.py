76ce4d50abf9ca377aaee05e72e84ab679da7a71
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-01-10 21:10:59 +0500

    Add files via upload

---
+++ 02_Shapes_And_Reshape/6_Shape.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[1,2,3,4]])
+print(arr)
+print()
+print(arr.shape)
+arr = np.array([1,2,3,4],ndmin = 4)
+print(arr)
+print()
+print(arr.shape)
---
+++ 02_Shapes_And_Reshape/7_ConversionOfARray.py
@@ -0,0 +1,15 @@
+# Reshape
+import numpy as np
+arr = np.array([1,2,3,4,5,6])
+print(arr)
+# print()
+arr = arr.reshape(3,2) # convert kerne se pehle dekh lena ke elemment itne mojood bhi hai ke reshape hojae and jitne chahe utni dimension lagale
+print(arr)
+print(arr.ndim)
+print(arr.reshape(6))
+print(arr.ndim)
+print(arr)
+print(arr.ndim)
+arr=arr.reshape(6)
+print(arr)
+print(arr.ndim)
