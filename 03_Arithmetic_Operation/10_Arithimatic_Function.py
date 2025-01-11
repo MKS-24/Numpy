495c2c7948867e62b89597759e4cf1d20e690e2e
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-01-12 00:40:00 +0500

    Add files via upload

---
+++ 03_Arithmetic_Operation/10_Arithimatic_Function.py
@@ -0,0 +1,21 @@
+import numpy as np
+arr = np.array([2,3,9,4,5,1,7,8])
+
+print(arr.max())
+print(np.max(arr))
+print(arr.argmax()) # poisition batataa max ki
+
+print(arr.min())
+print(np.min(arr))
+print(arr.argmin()) # poisition batataa min ki
+
+arr3 = np.array([[6,1,3],[5,6,7]])
+print(np.max(arr3,axis=1))
+print(arr3.max(axis=1)) # axis = 1 dono ke row check kerta hai firsr row fistrt matirix ka compare hoga second matrix ke first row se  
+print(arr3.min(axis=1))
+print(np.min(arr3,axis=1))
+
+print(np.max(arr3,axis=0))
+print(arr3.max(axis=0))
+print(arr3.min(axis=0))
+print(np.min(arr3,axis=0))  # axis = 0 dono ke colume check kerta hai firsr colume fistrt matirix ka compare hoga second matrix ke first colume se  
