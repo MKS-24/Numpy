9467bd04aca350fc3014bd1d81ae5dfc674e3f35
Muhammad Khuzaima Siddiqui <mksprogramify@gmail.com>
2025-01-11 23:47:50 +0500

    Add files via upload

---
+++ 02_Shapes_And_Reshape/6_Shape.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr = np.array([[1,2,3,4],[1,2,3,4],[1,2,3,4]])
+print(arr)
+print()
+print(arr.shape)
+arr = np.array([1,2,3,4],ndmin = 4)
+arr = np.array([[[1,2,3,4],[1,2,3,4]],[[1,2,3,4],[1,2,3,4]]],ndmin = 4)
+print(arr)
+print()
+print(arr.shape)
